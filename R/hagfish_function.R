7ee5ee3768f1659aabeec882085a0d4d558dc040
roopakrithi <roopa.krithi@gmail.com>
2017-09-05 21:28:04 -0400

    first commit. hagfish.

---
+++ R/hagfish_function.R
@@ -0,0 +1,17 @@
+#' A hagfish function
+#'
+#' This function allows you to express your love of hagfish.
+#' @param love Do you love hagfish? Defaults to TRUE.
+#' @keywords hagfish
+#' @export
+#' @examples
+#' hagfish_function()
+
+hagfish_function <- function(love=T){
+  if (love == T){
+    print("hagfish need love too")
+  }
+  else {print ("hagfish are weird")
+  }
+}
+
